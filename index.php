039125e58c1a9d1914d3e67489e3a6a22d59cf02
Byazit <byazit@gmail.com>
2019-05-15 16:47:28 +0200

    test2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="css/custom.foundation.min.css">
+<title>All my project will be here</title>
+</head>
+<body>
+	<div class='row'>
+		<div class='small-12 columns'>Wonderful</div>
+	</div>
+</body>
+</html>
