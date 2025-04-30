d3400848bc066ada7c52a35d34b4aa26c7d8b07f
byazit <byazit@gmail.com>
2025-04-30 17:26:24 +0200

    fresh start

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,35 +11,52 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-    axios.get('https://www.omdbapi.com/?s=' + searchText + '&apikey=' + API_KEY)
+    axios.get(`https://www.omdbapi.com/?s=${searchText}&apikey=${API_KEY}`)
         .then((response) => {
             let movies = response.data.Search;
             let output = '';
 
-            $.each(movies, (index, movie) => {
-                output += `
-                    <div class="card">
-                        <div class="card-image waves-effect waves-block waves-light">
-                        <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/300x450.png?text=No+Image'}">
-                        </div>
-                        <div class="card-content">
-                        <span class="card-title">${movie.Title}</span>
-                        </div>
-                        <div class="card-action">
-                        <a onclick="movieSelected('${movie.imdbID}')" href="#">Movie Details</a>
-                        </div>
-                    </div>
-                    `;
+            if (!movies) {
+                $('#movies').html('<p class="red-text">No movies found.</p>');
+                return;
+            }
+
+            // For each movie, fetch full details
+            let requests = movies.map((movie) => {
+                return axios.get(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`);
             });
 
-            $('#movies').html(output);
+            Promise.all(requests).then(responses => {
+                responses.forEach((res) => {
+                    let movie = res.data;
+                    output += `
+                        <div class="col s12 m6 l3">
+                          <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                              <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/300x450.png?text=No+Image'}">
+                            </div>
+                            <div class="card-content">
+                              <span class="card-title">${movie.Title}</span>
+                              <p><strong>Year:</strong> ${movie.Year}</p>
+                              <p><strong>Genre:</strong> ${movie.Genre}</p>
+                              <p><strong>Director:</strong> ${movie.Director}</p>
+                              <p><strong>Actors:</strong> ${movie.Actors}</p>
+                              <p><strong>Plot:</strong> ${movie.Plot.length > 100 ? movie.Plot.slice(0, 100) + '...' : movie.Plot}</p>
+                            </div>
+                          </div>
+                        </div>
+                    `;
+                });
 
+                $('#movies').html(output);
+            });
         })
         .catch((err) => {
             console.log(err);
             $('#movies').html('<p class="red-text">Error fetching movies.</p>');
         });
 }
+
 $(document).ready(() => {
     // Trigger search when clicking the button
     $('#searchBtn').on('click', (e) => {
