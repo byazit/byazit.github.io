ad3432aef5a4ad62e8a0e93984bc213542d829cd
byazit <byazit@gmail.com>
2025-04-30 17:15:16 +0200

    fresh start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,61 @@
 let API_KEY = 'b3844d55';
 
 $(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
+    // Trigger search when clicking the button
+    $('#searchBtn').on('click', () => {
         let searchText = $('#searchText').val();
-        getMovies(searchText);
-        e.preventDefault();
+        if (searchText.trim() !== '') {
+            getMovies(searchText);
+        }
     });
-      
 });
 
-
-function getMovies(searchText){
-    axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=b3844d55')
+function getMovies(searchText) {
+    axios.get('http://www.omdbapi.com/?s=' + searchText + '&apikey=' + API_KEY)
         .then((response) => {
-            console.log(response);
             let movies = response.data.Search;
-            let output ='';
+            let output = '';
+
             $.each(movies, (index, movie) => {
                 output += `
-                <div class ="colMovie">
-                    <div class="well text-center">
-                        <img src="${movie.Poster}">
-                        <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                    </div> 
-                </div>
-                `;
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/300x450.png?text=No+Image'}">
+    </div>
+    <div class="card-content">
+      <span class="card-title">${movie.Title}</span>
+    </div>
+    <div class="card-action">
+      <a onclick="movieSelected('${movie.imdbID}')" href="#">Movie Details</a>
+    </div>
+  </div>
+`;
             });
-            $('#movies').html(output);
-        })
-    
-        .catch((err) =>{
-            console.log(err);
-        })
-    }
-    window.onload = function(){
-        axios.get('http://www.omdbapi.com/?s='+'Action'+'&apikey=b3844d55')
-        .then((response) => {
-            console.log(response);
-            let movies = response.data.Search;
-            let output ='';
-            $.each(movies, (index, movie) => {
-                output += `
-                <div class ="colMovie">
-                    <div class="well text-center">
-                        <img src="${movie.Poster}">
-                        <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                    </div> 
-                </div>
-                `;
-            });
-            $('#movies').html(output);
-        })
-    
-        .catch((err) =>{
-            console.log(err);
-        })
 
-    }
+            $('#movies').html(output);
+
+        })
+        .catch((err) => {
+            console.log(err);
+            $('#movies').html('<p class="red-text">Error fetching movies.</p>');
+        });
+}
+$(document).ready(() => {
+    // Trigger search when clicking the button
+    $('#searchBtn').on('click', (e) => {
+        e.preventDefault();
+        let searchText = $('#searchText').val();
+        if (searchText.trim() !== '') {
+            getMovies(searchText);
+        }
+    });
+
+    // Trigger search on Enter key press
+    $('#searchForm').on('submit', (e) => {
+        e.preventDefault(); // Prevent default form submission
+        let searchText = $('#searchText').val();
+        if (searchText.trim() !== '') {
+            getMovies(searchText);
+        }
+    });
+});
